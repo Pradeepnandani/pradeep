e3d83351491ed53851144d5793f76f96d38efa30
Pradeepnandani <95478281+Pradeepnandani@users.noreply.github.com>
2022-01-27 10:56:27 +0530

    Add files via upload

---
+++ Project/Encrypt_Decrypt_Project/encrypt_decrypt.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace encrypydecrypt
+{
+    class encrypt_decrypt
+    {
+        public static string encryptstring(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream((Stream)ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter((Stream)cs))
+                        {
+                            sw.Write(plainText);
+                        }
+                        array = ms.ToArray();
+                    }
+                }
+
+
+            }
+
+            return (Convert.ToBase64String(array));
+        }
+
+        public static string decryptor(string key, string ciphertext)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(ciphertext);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
